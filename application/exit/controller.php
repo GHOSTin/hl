754a9255b8194518b6d4ce18fec998c6ce5efed4
nekrasov <nekrasov@mlsco.ru>
2013-02-19 17:32:08 +0600

    Поправил контроллер выхода из приложения

--- application/exit/controller.php
+++ application/exit/controller.php
@@ -1,15 +1,8 @@
 <?php
 class controller_exit{
-	
 	public static function private_show_default_page(){
-		//unset($_SESSION['user_id']);
-		//unset($_SESSION['id']);
 		session_destroy();
 		header('Location:/');
 		exit();
 	}
-	
-	public static function public_show_default_page(){
-		self::private_show_default_page();
-	}	
 }
