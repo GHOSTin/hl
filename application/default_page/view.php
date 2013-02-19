2bc1ebea31eef19e50f93a0c63fb3f1bc9c3842a
nekrasov <nekrasov@mlsco.ru>
2013-02-19 17:14:08 +0600

    Сделал чтобы выводилась страница по умолчанию

--- application/default_page/view.php
+++ application/default_page/view.php
@@ -1,7 +1,6 @@
 <?php
 class view_default_page{
-
-	public static function show_default_page(){
+	public static function private_show_default_page(){
 		return load_template('default_page.show_default_page');
 	}
 }
