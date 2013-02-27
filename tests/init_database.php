efe1a65be8fea0678247baae55c8ea771b692792
nekrasov <nekrasov@mlsco.ru>
2013-02-27 11:20:47 +0600

    Скрипт инициализации создает пользователя.

--- tests/init_database.php
+++ tests/init_database.php
@@ -1,20 +1,28 @@
 <?php
+set_time_limit(0);
 # подключаем фреймворк
 $dir = dirname(__FILE__);
 define('ROOT' , substr($dir, 0, (strlen($dir) - strlen('/test'))));
 require_once(ROOT."/framework/framework.php");
-try{
-	model_environment::create_batabase_connection();
-	drop_tables();
-	create_tables();
-	
-}catch(exception $e){
-
-	die($e->getMessage());
-}
-
+/*
+* Создает таблицы
+*/
 function create_tables(){
 	$stm = db::get_handler()->exec(file_get_contents(ROOT."/specifications/database_structure.sql"));
+}
+/*
+* Создает фейковых юзеров
+*/
+function create_users(){
+	$sql = "INSERT INTO `users` (
+				`id`, `company_id`,`status`, `username`, `firstname`, `lastname`,
+				`midlename`, `password`, `telephone`, `cellphone`
+			) VALUES (
+				1, 1, true, 'NekrasovEV', 'Евгений', 'Некрасов', 'Валерьевич', 
+				'".get_password_hash('{htyfntym')."', 647957, '+7 922 294 47 42'
+			);";
+	$stm = db::get_handler()->prepare($sql);
+	$stm->execute();
 }
 /*
 * Стираем все таблицы в базе данных
@@ -28,5 +36,17 @@ function drop_tables(){
 	if(!empty($table_string))
 		$stm = db::get_handler()->exec('DROP TABLES '.substr($table_string, 0, -2));
 }
-
-	
+function get_password_hash($passwd){
+	return md5(md5(htmlspecialchars($passwd)).application_configuration::authSalt);
+}
+/*
+* Запуск
+*/
+try{
+	model_environment::create_batabase_connection();
+	drop_tables();
+	create_tables();
+	create_users();
+}catch(exception $e){
+	die($e->getMessage());
+}
