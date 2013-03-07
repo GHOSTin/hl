822c62696ab7f679e213183f00ebd1d93dc829db
nekrasov <nekrasov@mlsco.ru>
2013-03-07 14:32:33 +0600

    Сделал графическую составляющую для вывода доступа.

---
+++ application/error/controller.php
@@ -0,0 +1,9 @@
+<?php
+class controller_error{
+	public static function private_show_default_page(){
+		die('Нет страницы.');
+	}
+	public static function private_get_access_denied_message(){
+		return true;
+	}
+}
--- application/error/view.php
+++ application/error/view.php
@@ -1,7 +1,9 @@
 <?php
 class view_error{
-
 	public static function get_error_message($message){
 		return load_template('error.private_get_error_message', ['message' => $message]);
+	}
+	public static function private_get_access_denied_message(){
+		return 'Нет доступа!';
 	}
 }
