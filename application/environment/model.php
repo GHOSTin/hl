7173c61684e549a9463ea793118a37fc5f0439f7
nekrasov <nekrasov@mlsco.ru>
2013-03-08 13:12:26 +0600

    Перенес функциональность в модель профиль.

--- application/environment/model.php
+++ application/environment/model.php
@@ -58,21 +58,6 @@ class model_environment{
 		}
 	}
 	/*
-	* Проверяет права доступа пользователя
-	*/
-	private static function check_general_access($controller, $component){
-		try{
-			if(property_exists($controller, 'rules')){
-				if($_SESSION['rules'][$component]->generalAccess !== true)
-					return false;
-					return true;
-			}else
-				return true;
-		}catch(exception $e){
-			throw new exception('Проблема при проверке прав доступа.');
-		}
-	}
-	/*
 	* Функция возвращает содержимое страницы
 	*/
 	public static function get_page_content(){
@@ -83,8 +68,8 @@ class model_environment{
 			$view = 'view_'.$component;
 			self::load_twig();
 			if($_SESSION['user'] instanceof data_user){
-				self::get_user_profiles();
-				$access = (self::check_general_access($controller, $component));
+				model_profile::get_user_profiles();
+				$access = (model_profile::check_general_access($controller, $component));
 				if($access !== true){
 					$controller = 'controller_error';
 					$view = 'view_error';
@@ -100,38 +85,6 @@ class model_environment{
 			return self::build_page($data);
 		}catch(exception $e){
 			return $e->getMessage();
-		}
-	}
-	/**
-	* Проверяет залогинен ли пользователь
-	* @return bolean
-	*/
-	private static function get_user_profiles(){
-		try{
-			$sql = "SELECT `profile`, `rules`, `restrictions`, `settings`
-				FROM `profiles`
-				WHERE  `user_id` = :user_id
-				AND `company_id` = :company_id";
-			$stm = db::get_handler()->prepare($sql);
-			$stm->bindValue(':user_id', $_SESSION['user']->id , PDO::PARAM_INT);
-			$stm->bindValue(':company_id',$_SESSION['user']->company_id , PDO::PARAM_INT);
-			if($stm->execute() == false)
-				throw new exception('Ошибка при получении профиля.');
-			if($stm->rowCount() > 0){
-				while($profile = $stm->fetch()){
-					$_SESSION['rules'][$profile['profile']] = json_decode($profile['rules']);
-					$_SESSION['restrictions'][$profile['profile']] = json_decode($profile['restrictions']);
-					$_SESSION['settings'][$profile['profile']] = json_decode($profile['settings']);
-					if($_SESSION['rules'][$profile['profile']]->generalAccess === true){
-						$c = 'controller_'.$profile['profile'];
-						$links[] = ['href' => $profile['profile'], 'title' => $c::name];
-					}
-					$_SESSION['menu'] = $links;
-				}
-			}
-			$stm->closeCursor();
-		}catch(exception $e){
-			throw new exception('Ошибка при получении профиля.');
 		}
 	}
 	/*
---
+++ application/profile/model.php
@@ -0,0 +1,50 @@
+<?php
+class model_profile{
+	/**
+	* Проверяет залогинен ли пользователь
+	* @return bolean
+	*/
+	public static function get_user_profiles(){
+		try{
+			$sql = "SELECT `profile`, `rules`, `restrictions`, `settings`
+				FROM `profiles`
+				WHERE  `user_id` = :user_id
+				AND `company_id` = :company_id";
+			$stm = db::get_handler()->prepare($sql);
+			$stm->bindValue(':user_id', $_SESSION['user']->id , PDO::PARAM_INT);
+			$stm->bindValue(':company_id',$_SESSION['user']->company_id , PDO::PARAM_INT);
+			if($stm->execute() == false)
+				throw new exception('Ошибка при получении профиля.');
+			if($stm->rowCount() > 0){
+				while($profile = $stm->fetch()){
+					$_SESSION['rules'][$profile['profile']] = json_decode($profile['rules']);
+					$_SESSION['restrictions'][$profile['profile']] = json_decode($profile['restrictions']);
+					$_SESSION['settings'][$profile['profile']] = json_decode($profile['settings']);
+					if($_SESSION['rules'][$profile['profile']]->generalAccess === true){
+						$c = 'controller_'.$profile['profile'];
+						$links[] = ['href' => $profile['profile'], 'title' => $c::name];
+					}
+					$_SESSION['menu'] = $links;
+				}
+			}
+			$stm->closeCursor();
+		}catch(exception $e){
+			throw new exception('Ошибка при получении профиля.');
+		}
+	}
+	/*
+	* Проверяет права доступа пользователя
+	*/
+	public static function check_general_access($controller, $component){
+		try{
+			if(property_exists($controller, 'rules')){
+				if($_SESSION['rules'][$component]->generalAccess !== true)
+					return false;
+					return true;
+			}else
+				return true;
+		}catch(exception $e){
+			throw new exception('Проблема при проверке прав доступа.');
+		}
+	}	
+}
