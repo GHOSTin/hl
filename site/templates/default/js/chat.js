bd4032939a9b545c5cafb673ca3c3d460c7668d7
GHOST work <lwhiteghostl@gmail.com>
2013-03-27 14:06:31 +0500

    сделал коментарии к функционалу чата

--- site/templates/default/js/chat.js
+++ site/templates/default/js/chat.js
@@ -1,20 +1,33 @@
+/**
+ * @namespace экземпляр userList
+ * @type {{list: {}, load: Function, renderMenu: Function, renderPanes: Function, clearOnLoad: Function}}
+ */
 window.userList = {
     list:{},
+    /**
+     * Создает массив пользователей
+     * @this {userList}
+     * @param {Array} list Список атрибутов пользователей
+     */
     load:function (list) {
         for (var id in list) {
             var user_attributes = list[id];
-            this.list[user_attributes.id] = new User(user_attributes);
+            /** @private */ this.list[user_attributes.id] = new User(user_attributes);
         }
         this.clearOnLoad();
         this.renderMenu();
         this.renderPanes();
     },
+    /**
+     * Создает рендер списка пользователей
+     * @this {userList}
+     */
     renderMenu:function () {
         var activeTabId = $('.users li.active a').attr('user_id');
         $('.users li').remove();
-        var online = '',  //список онлайн
-            offline = '', //список оффлайн
-            unread = '';  //список тех у кого есть непрочитанные сообщения
+        var online = '',  /** {String} Список онлайн*/
+            offline = '', /** {String} список оффлайн */
+            unread = '';  /** {String} список тех у кого есть непрочитанные сообщения*/
         for (var id in this.list) {
             var user = this.list[id];
             var online_status = ((user.online) ? 'online' : 'offline');
@@ -32,6 +45,10 @@ window.userList = {
         $('.users').append(unread + online + offline);
         $('a[user_id=' + activeTabId + "]").parent().addClass('active');
     },
+    /**
+     * Создание рендера блоков переписки для каждого пользователя
+     * @this {userList}
+     */
     renderPanes:function () {
         var list = this.list;
         for (var i in this.list) {
@@ -61,24 +78,32 @@ window.userList = {
     clearOnLoad:function () {
     }
 };
-
+/**
+ * @namespace экземпляр Feed
+ * @type {{process: Function, formatDate: Function, addStatus: Function, urlify: Function}}
+ */
 window.feed = {
+    /**
+     * Функция обработки обновлений контента
+     * @function
+     * @param {Array} updates - массив обновлений формата [code,...,params]
+     */
     process:function (updates) {
         for (var id in updates) {
             var update = updates[id];
             var code = update.shift();
             switch (code) {
-                case 3: //отметить сообщение как прочитанное
+                case 3: /** @lends отметить сообщение как прочитанное */
                     var message_id = update.shift();
                     var flags = update.shift();
                     var user_id = update.shift();
 
                     if (flags & 1) userList.list[user_id].messages[message_id].read();
                     break;
-                case 4: //пришло новое сообщение
+                case 4: /** @lends пришло новое сообщение */
                     var message = new Message(update);
                     break;
-                case 5: //кол-во не прочитанных сообщений
+                case 5: /** @lends кол-во не прочитанных сообщений */
                     var user_id = update[0];
                     var user = userList.list[user_id];
                     if(typeof user !== 'undefined' && user !== null) {
@@ -86,14 +111,14 @@ window.feed = {
                         userList.renderMenu();
                     }
                     break;
-                case 6: //подгрузка списка дат
+                case 6: /** @lends подгрузка списка дат */
                     new History_dates(update);
                     break;
-                case 7: //загрузка истории
+                case 7: /** @lends загрузка истории */
                     new History(update);
                     break;
-                case 1: //пользователь онлайн
-                case 2: //пользователь оффлайн
+                case 1: /** @lends пользователь онлайн */
+                case 2: /** @lends пользователь оффлайн */
                     var user_id = update[0];
                     var user = userList.list[user_id];
                     if(typeof user !== 'undefined' && user !== null){
@@ -101,14 +126,20 @@ window.feed = {
                         userList.renderMenu();
                     }
                     break;
-                default:
+                default: /** @lends если код не был обнаружен */
                     log('received unknown update:');
                     log(update);
                     break;
             }
         }
     },
-
+    /**
+     * Функция преобразования даты/время
+     * @function
+     * @param {Number} _date Значение timestamp
+     * @param {String} _status "date" - если нужно вернуть дату, 'time' - если время
+     * @returns {string} возвращает преобразованную строку с датой/временем
+     */
     formatDate:function (_date, _status) {
         var status = _status || 'time';
         var date = (typeof _date!=='undefined' && _date !==null)?new Date(_date):new Date();
@@ -150,11 +181,21 @@ window.feed = {
             break;
         }
     },
-
+    /**
+     * Функция прикрепляет уведомление в панель чата о состоянии пользователя
+     * @deprecated Убрана в версии mshc 2.0, т.к. занимала много места
+     * @param {string} statusString "В сети"/"Вышел из сети"
+     * @param {User} user Пользователь статус которого изменился
+     */
     addStatus:function (statusString, user) {
         $('#user_'+user.id+' div.feed ul#dates').append("<li>" + statusString + "</li>");
     },
-
+    /**
+     * @function
+     * Ищет есть ли в тексте url и заменяет их на ссылки
+     * @param {string} text Текст который нужно преобразовать
+     * @returns {XML|string|void} Преобразованные url
+     */
     urlify:function(text){
         var urlRegex = /(https?:\/\/[^\s]+)/g;
         return text.replace(urlRegex, function(url) {
@@ -162,7 +203,11 @@ window.feed = {
         });
     }
 };
-
+/**
+ * Создает экземпляр User
+ * @param {Array} data JSON-массив входящих параметров пользователя
+ * @constructor
+ */
 var User =
     function (data) {
         this.id = data.id;
@@ -173,7 +218,11 @@ var User =
         this.history_dates = {};
         this.previousMessagesLoaded = false;
     };
-
+/**
+ * Загружает предыдущие сообщения
+ * @param {Array} messages массив сообщений
+ * @param {string} status по умолчанию "history"
+ */
 User.prototype.loadPreviousMessages = function (messages, status) {
     if (typeof messages !== "undefined" && messages !== null) {
         for (var id in messages) {
@@ -202,20 +251,32 @@ User.prototype.loadPreviousMessages = function (messages, status) {
         chat.json.send({'type':'load_previous_messages', 'data':{"uid":this.id}});
     }
 };
-
+/**
+ * запрашивает кол-во непрочитанных сообщений
+ * @returns {Number} кол-во непрочитанных сообщений
+ */
 User.prototype.unreadCount = function () {
     if (this.previousMessagesLoaded) return this.unreadMessagesIds().length;
     return this.unread;
 };
-
+/**
+ * проверяет есть ли непрочитанные сообщения
+ * @returns {boolean}
+ */
 User.prototype.hasUnread = function () {
     return (this.unreadCount() > 0) ? true : false;
 };
-
+/**
+ * проверка на активность текущей панели пользователя
+ * @returns {boolean}
+ */
 User.prototype.paneActive = function () {
     return ($("a[user_id=" + this.id + "]").parent().hasClass('active')) ? true : false;
 };
-
+/**
+ * получает массив id непрочитанных сообщений
+ * @returns {Array}
+ */
 User.prototype.unreadMessagesIds = function () {
     var _results;
     _results = [];
@@ -227,7 +288,10 @@ User.prototype.unreadMessagesIds = function () {
     }
     return _results;
 };
-
+/**
+ * добавляет новое сообщение
+ * @param {Message} message - объект класса Message
+ */
 User.prototype.addMessage = function (message) {
     this.messages[message.id] = message;
     if (message.unreadAndIncoming()) {
@@ -236,17 +300,26 @@ User.prototype.addMessage = function (message) {
     }
     if (this.paneActive()) this.markAllAsRead();
 };
-
+/**
+ * добавляет в список истории дат новую дату
+ * @param {History_dates} history - объект класса History_date
+ */
 User.prototype.addHistory_date = function (history) {
     this.history_dates[history.id] = history;
 }
-
+/**
+ * отмечает все непрочитанные сообщения, как прочитанные
+ */
 User.prototype.markAllAsRead = function () {
     if (this.previousMessagesLoaded && this.hasUnread()) {
         chat.json.send({'type':'mark_as_read', 'data':{'mids':this.unreadMessagesIds().join(','), 'uid':this.id}});
     }
 };
-
+/**
+ * создает экземпляр History_dates
+ * @param {Array} params - массив параметров объекта [id, user_id]
+ * @constructor
+ */
 var History_dates =
     function(params){
         this.id = params[0];
@@ -258,7 +331,9 @@ var History_dates =
         this.user.addHistory_date(this);
         this.render();
     };
-
+/**
+ * создает рендер блока даты для истории сообщений
+ */
 History_dates.prototype.render = function(){
     var history_date = '';
     history_date += '<section>';
@@ -269,15 +344,24 @@ History_dates.prototype.render = function(){
     $("#user_" + this.user.id + " div.feed ul#history").prepend(history_date);
     $("#user_" + this.user.id + " div.feed").mCustomScrollbar("update");
 };
-
+/**
+ * добавляет сообщение из истории в конкретную дату
+ * @param {History} history - объект класса history
+ */
 History_dates.prototype.addHistory = function(history){
     this.history[history.id] = history;
 };
-
+/**
+ * очищает конкретную дату сообщений
+ */
 History_dates.prototype.clear = function(){
+    this.history = {};
     $('header#'+this.id).siblings('ul').empty();
 };
-
+/**
+ * загружает сообщения за конкретную дату
+ * @param {Array} messages - массив сообщений
+ */
 History_dates.prototype.loadHistory = function (messages) {
     if (typeof messages !== "undefined" && messages !== null) {
         this.clear();
@@ -301,7 +385,11 @@ History_dates.prototype.loadHistory = function (messages) {
         chat.json.send({'type':'get_history_day', 'data':{"uid":this.user.id, "date": this.id}});
     }
 };
-
+/**
+ * создает экземпляр History
+ * @param {Array} params - массив параметров объекта [id, flags, user_id, date, time, text]
+ * @constructor
+ */
 var History =
     function(params){
         this.id = params[0];
@@ -314,7 +402,9 @@ var History =
         this.date.addHistory(this);
         this.render();
     };
-
+/**
+ * создает рендер блока даты для истории сообщений
+ */
 History.prototype.render = function(){
     var classes = ['message'];
     (this.outgoing)? classes.push('pull-right'): classes.push('pull-left');
@@ -328,9 +418,13 @@ History.prototype.render = function(){
 
     $('header#'+this.date.id).siblings('ul').append(message_string);
 };
-
+/**
+ * создает экземпляр Message
+ * @param {Array} params массив параметров объекта [id, flags, from_id, timestamp, subject, text, attachments, status]
+ * @constructor
+ */
 var Message =
-    function (params/* id, flags, from_id, timestamp, subject, text, attachments, status*/) {
+    function (params) {
         this.id = params[0];
         this.status = params[7] || 'new';
         this.subject = params[4];
@@ -344,11 +438,16 @@ var Message =
         this.user.addMessage(this);
         this.render();
     };
-
+/**
+ * проверяет на соответствие условиям, что сообщение непрочитано и входящее
+ * @returns {boolean}
+ */
 Message.prototype.unreadAndIncoming = function () {
     return (this.unread && !this.outgoing);
 };
-
+/**
+ * отмечает сообщение как прочитанное
+ */
 Message.prototype.read = function () {
     this.unread = false;
 
@@ -357,7 +456,9 @@ Message.prototype.read = function () {
         userList.renderMenu();
     }
 };
-
+/**
+ * создает рендер сообщения
+ */
 Message.prototype.render = function () {
     var classes = ['message'];
     (this.outgoing)? classes.push('pull-right'): classes.push('pull-left');
@@ -391,13 +492,19 @@ Message.prototype.render = function () {
         this.prepend(message_string);
     else this.append(message_string);
 };
-
+/**
+ * вызывается если сообщение является историей
+ * @param {string} render - рендер сообщения
+ */
 Message.prototype.prepend = function(render) {
     $("#user_" + this.user.id + " div.feed ul#dates section:first ul").prepend(render);
     $("#user_" + this.user.id + " div.feed").mCustomScrollbar("update");
     $("#user_" + this.user.id + " div.feed").mCustomScrollbar("scrollTo",'blockquote#'+this.user.first_message_id);
 };
-
+/**
+ * вызывается если сообщение новое
+ * @param {string} render - рендер сообщения
+ */
 Message.prototype.append = function(render) {
     $("#user_" + this.user.id + " div.feed ul#dates section:last ul").append(render);
     $("#user_" + this.user.id + " div.feed").mCustomScrollbar("update");
--- site/templates/default/js/notification-center.js
+++ site/templates/default/js/notification-center.js
@@ -1,10 +1,19 @@
+/**
+ * выводит в лог переданное значение
+ * @function
+ * @param {*} param
+ */
 var log = function (param) {
     console.log(param);
 };
-
+/** сокет-соединение для центра уведомлений */
 var notify_center = io.connect('http://192.168.2.202:3000/notify');
+/** сокет-соединение для чата */
 var chat = io.connect('http://192.168.2.202:3000/chat');
-
+/**
+ * если сокет создан, но не произошло соединение, то совершать пересоединение
+ * @function
+ */
 var tryReconnect = function(){
      if (notify_center.socket.connected === false &&
          notify_center.socket.connecting === false) {
@@ -12,8 +21,15 @@ var tryReconnect = function(){
          chat.socket.connect();
      }
 }
-
+/**
+ * с интервалом раз в минуту вызывать функцию tryReconnect
+ * @function
+ * @type {number}
+ */
 var intervalID = setInterval(tryReconnect, 60000);
+/**
+ * обработка события центра уведомлений "если произошло соединение"
+ */
 notify_center.on('connect', function(){
     if($('.current_user').length){
         notify_center.json.send({"type":"user_ready", "data":{'uid': parseInt($('.current_user').attr('user_id'))}});
@@ -21,7 +37,9 @@ notify_center.on('connect', function(){
     }
     clearInterval(intervalID);
 });
-
+/**
+ * обработка события центра уведомлений "если пришло сообщение"
+ */
 notify_center.on('message', function(event){
     var message = event.data;
     switch (message.type) {
@@ -35,31 +53,39 @@ notify_center.on('message', function(event){
             console.log(message);
     }
 });
+/**
+ * обработка события центра уведомлений "если соединение было разорвано"
+ */
 notify_center.on('disconnect', function(){
     intervalID = setInterval(tryReconnect, 60000);
 });
-
+/**
+ * обработка события чата "если пришло сообщение"
+ */
 chat.on('message', function (event) {
     var message = event.data;
     switch (message.type) {
-        case 'previous_messages':
+        case 'previous_messages': /** сщщбщение - присланы предыдущие сообщения */
             userList.list[message.data.uid].loadPreviousMessages(message.data.messages, message.data.status || 'new');
             break;
-        case 'updates':
+        case 'updates': /** сообщение - произвести обновления */
             feed.process(message.data);
             break;
-        case 'history':
+        case 'history': /** сообщение - загрузка истории сообщений */
             userList.list[message.data.uid].history_dates[message.data.date].loadHistory(message.data.messages);
             break;
-        default:
+        default: /** неизвестный тип сообщения */
             log('received unknown message:');
             log(message);
     }
 });
-
+/**
+ * произведен клик на имя пользователя
+ */
 $(document).on('click', '.users li a', function (e) {
     e.preventDefault();
     var user = userList.list[$(this).attr("user_id")];
+    /** если у пользователя не были подгружены последние 15 сообщений*/
     if (!user.previousMessagesLoaded) {
         user.loadPreviousMessages();
     }
@@ -68,7 +94,9 @@ $(document).on('click', '.users li a', function (e) {
     $('.chat.active .feed').mCustomScrollbar("update");
     $('.chat.active .feed').mCustomScrollbar("scrollTo", "bottom");
 });
-
+/**
+ * произведена отправка формы
+ */
 $(document).on('submit', 'form.message', function (e) {
     var form = $(e.target);
     var files = [];
@@ -83,7 +111,9 @@ $(document).on('submit', 'form.message', function (e) {
     form[0].message.value = "";
     return false;
 });
-
+/**
+ * обработка события нажатия клавиш Ctrl+Enter
+ */
 $(document).on('keypress', '.chat.active textarea',function (event) {
     var keyCode = (event.which ? event.which : event.keyCode);
 
@@ -91,9 +121,12 @@ $(document).on('keypress', '.chat.active textarea',function (event) {
         $(this).parent().submit();
     }
 });
-
+/**
+ * обработка события нажатия на кнопку показа истории
+ */
 $(document).on('click', '.history', function(e){
     e.preventDefault();
+    /** если клавиша не была нажата, то подгрузить даты */
     if($(this).hasClass('active')){
         var user = userList.list[$('.chat.active').attr('id').split('_')[1]];
         chat.json.send({'type':'get_history_list', 'data':{"uid": user.id, "offset": Object.keys(user.history_dates).length}});
@@ -105,7 +138,9 @@ $(document).on('click', '.history', function(e){
         $('.chat.active .feed').mCustomScrollbar("update");
     });
 });
-
+/**
+ * обработка события нажатия на конкретную дату в истории
+ */
 $(document).on('click', '.chat.active ul#history header', function(e){
     e.preventDefault();
     if($(this).siblings('ul').is(':hidden')){
@@ -120,24 +155,33 @@ $(document).on('click', '.chat.active ul#history header', function(e){
         $(".chat.active .feed").mCustomScrollbar("update");
     });
 });
-
+/**
+ * обработка события нажатия на кнопку центра уведомлений
+ */
 $('#nt-center').on('click', function(e){
     e.preventDefault();
+    /** если существует то показать/скрыть */
     if($('#_hidden_notification_center').length){
         $('#_hidden_notification_center').fadeToggle(10,function(){
             $(".user-list").mCustomScrollbar("update");
         });
     } else {
+        /** подгрузить контент */
         $.get('/profile/get_notification_center_content',{
         },function(r){
+            /** создать блок с полученным контентом */
             create_notification_center($('#nt-center').parent(), r);
             var users = [];
             $('.users li').each(function(){
                 users.push({"id": $(this).attr('user_id'), "name": $(this).text()});
             });
+            /** передает список пользователей полученных из контента */
             userList.load(users);
+            /** запрос на получение списка онлайн пользователей */
             chat.json.send({'type':'get_users_list'});
+            /** запрос на получение кол-ва непрочитанных сообщений */
             chat.json.send({'type':'get_unread_messages'});
+            /** подключение custom скролла */
             $('.user-list').mCustomScrollbar({
                 scrollButtons:{
                     enable: true
@@ -156,6 +200,7 @@ $('#nt-center').on('click', function(e){
                 autoHideScrollbar: true,
                 scrollInertia: 0,
                 callbacks:{
+                    /** реализация бесконечной прокрутки сообщений/истории по датам */
                     onTotalScrollBack: function(){
                         if($('.chat.active').length){
                             var active_user = userList.list[$('.chat.active').attr('id').split('_')[1]];
@@ -177,7 +222,12 @@ $('#nt-center').on('click', function(e){
     }
     $('.notification-center-icon').removeClass('icon-white');
 });
-function create_notification_center(selector, r){
+/**
+ * создает блок для центра уведомлений
+ * @param selector - селектор к которому нужно привизать блок
+ * @param content - контент блока
+ */
+function create_notification_center(selector, content){
     $('#_hidden_notification_center').remove();
-    selector.append('<div id="_hidden_notification_center">' + r + '</div>');
+    selector.append('<div id="_hidden_notification_center">' + content + '</div>');
 }
