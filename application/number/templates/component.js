7a7331a040bd0bacfbbafdbac80de8121b593c28
GHOST work <lwhiteghostl@gmail.com>
2013-04-13 18:45:24 +0600

    работа над фильтрацией жилфонда

--- application/number/templates/component.js
+++ application/number/templates/component.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
             },function(r){
                 init_content(r);
             });
+        $('#filter-numbers').prepend('<span class="badge badge-success">'+$(this).text()+'</span>');
     });
     $('body').on('click', '.get_house_content', function(){
         $.get('get_house_content',{
@@ -12,6 +13,12 @@ $(document).ready(function(){
             },function(r){
                 init_content(r);
             });
+    });
+    $("#search-number").typeahead({
+        source: function(){var arr=[]; $('.streets li').each(function(){
+                    arr.push($(this).children('a').text());
+                });
+                return arr;}()
     });
 });
 function get_query_id(obj){
