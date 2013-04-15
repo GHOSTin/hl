ce62daf570172f5eb14465a26211dd89a176aa5a
GHOST work <lwhiteghostl@gmail.com>
2013-04-15 19:58:38 +0600

    фильтр по улице и по дому.

--- application/number/templates/component.js
+++ application/number/templates/component.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
             },function(r){
                 init_content(r);
             });
-        $('#filter-numbers').prepend('<span class="badge badge-success">'+$(this).text()+'</span>');
+        $("#filter-numbers span.street").remove();
+        $("#filter-numbers").prepend('<span class="label street" street-id="'+$(this).parent().attr('street')+'">'+$(this).text()+'<a class="close" href="#">&times;</a></span>')
+        $("#filter-numbers #search-number").attr('filter', 'houses');
     });
     $('body').on('click', '.get_house_content', function(){
         $.get('get_house_content',{
@@ -13,12 +15,45 @@ $(document).ready(function(){
             },function(r){
                 init_content(r);
             });
+        $("#filter-numbers span.house").remove();
+        $("#filter-numbers span.street").after('<span class="label house" house-id="'+$(this).parent().attr('house')+'">'+$(this).text()+'<a class="close" href="#">&times;</a></span>');
     });
     $("#search-number").typeahead({
-        source: function(){var arr=[]; $('.streets li').each(function(){
-                    arr.push($(this).children('a').text());
-                });
-                return arr;}()
+        source: function(query, process) {
+            objects = [];
+            map = {};
+            var data = [];
+            switch ($('#search-number').attr('filter')){
+                case 'streets':
+                    data = [];
+                    $('.streets li').each(function(){
+                        data.push({"id": $(this).attr('street'), "label": $(this).children('a').text() });
+                    });
+                    break;
+                case 'houses':
+                    data = [];
+                    $('.houses li').each(function(){
+                        data.push({"id": $(this).attr('house'), "label": $(this).children('a').text().replace('дом №', '') });
+                    });
+                    break;
+            }
+            $.each(data, function(i, object) {
+                map[object.label] = object;
+                objects.push(object.label);
+            });
+            process(objects);
+        },
+        updater: function(item) {
+            switch ($('#search-number').attr('filter')){
+                case 'streets':
+                    $('ul.streets li[street="'+map[item].id+'"] a.get_street_content').click();
+                    break;
+                case 'houses':
+                    $('ul.houses li[house="'+map[item].id+'"] a.get_house_content').click();
+                    break;
+            }
+            return null;
+        }
     });
 });
 function get_query_id(obj){
--- application/number/templates/private_show_default_page.tpl
+++ application/number/templates/private_show_default_page.tpl
@@ -3,6 +3,6 @@
 {% block component %}
      {% include '@number/build_street_titles.tpl' %}
      <div class="span6" id="filter-numbers">
-         <input id="search-number">
+         <input id="search-number" filter="streets">
      </div>
 {% endblock component %}
