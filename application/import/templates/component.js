3a4fed059887bd8d89b8986b5bac91aee7f49935
GHOST work <lwhiteghostl@gmail.com>
2013-04-18 18:21:05 +0600

    сделал галочку, функцию на отправку данных по нажатию на кнопку залить

--- application/import/templates/component.js
+++ application/import/templates/component.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
                 init_content(r);
             });
     });
+    $(document).on('click', '#toggle_checkboxes', function(){
+        $('tbody input[type=checkbox]').prop('checked', ($(this).is(':checked')));
+    });
+    $(document).on('click', '#send_import_numbers', function(){
+        var data = [];
+        $('table.table > tbody > tr').each(function(){
+            if($(this).find('td:first-child input[type=checkbox]').is(':checked')){
+                data.push({
+                    'ls': $(this).find('td:nth-child(3)').text().replace('л/c №', '')
+                    ,'fio': ($(this).find('td:nth-child(4) select').length)?
+                                $(this).find('td:nth-child(4) select').val():
+                                $(this).find('td:nth-child(4)').text()
+                });
+            }
+        });
+        $.post('load_numbers', {'numbers': data});
+    });
 });
