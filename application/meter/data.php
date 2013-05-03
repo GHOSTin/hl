2bd7b8dd9a891bf2d33b87076cdcda11b4218070
nekrasov <nekrasov@mlsco.ru>
2013-05-03 17:07:14 +0600

    Добавил веотфткацию в счетчики.

--- application/meter/data.php
+++ application/meter/data.php
@@ -4,9 +4,9 @@
 */
 final class data_meter extends data_object{
     
+    public $checktime;
     public $id;
     public $name;
-    public $service;
     public $serial;
-    public $checktime;
+    public $service;
 }
--- application/meter/model.php
+++ application/meter/model.php
@@ -1,6 +1,13 @@
 <?php
 class model_meter{
 	/**
+	* Верификация вермени поверки счетчика
+	*/
+	public static function verify_meter_chektime(data_meter $meter){
+		if($meter->chektime < 0)
+			throw new e_model('Время поверки счетчика задан не верно.');
+	}
+	/**
 	* Верификация идентификатора счетчика
 	*/
 	public static function verify_meter_id(data_meter $meter){
@@ -8,10 +15,31 @@ class model_meter{
 			throw new e_model('Идентификатор счетчика задан не верно.');
 	}
 	/**
+	* Верификация названия счетчика
+	*/
+	public static function verify_meter_name(data_meter $meter){
+		if(empty($meter->name))
+			throw new e_model('Название счетчика задан не верно.');
+	}
+	/**
 	* Верификация серийного номера счетчика
 	*/
 	public static function verify_meter_serial(data_meter $meter){
 		if(empty($meter->serial))
 			throw new e_model('Серийный номер счетчика задан не верно.');
+	}
+	/**
+	* Верификация названия службы
+	*/
+	public static function verify_meter_service(data_meter $meter){
+		if(empty($meter->service))
+			throw new e_model('Название службы задано не верно.');
+	}
+	/**
+	* Верификация типа объекта счетчика
+	*/
+	public static function is_data_meter($meter){
+		if(!($meter instanceof data_meter))
+			throw new e_model('Возвращеный объект не является счетчиком.');
 	}
 }
