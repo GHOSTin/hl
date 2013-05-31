47a8c35b5e63a2002251fa2e0d9d46ec8953476b
nekrasov <nekrasov@mlsco.ru>
2013-05-31 11:06:59 +0600

    Сделал обновление для того что бы хранить справочные периоды поверки.

--- update.php
+++ update.php
@@ -30,6 +30,10 @@ try{
     $sql->execute('Столбец rates не был создан.');
 
     $sql = new sql();
+    $sql->query("ALTER TABLE `meters` add COLUMN `time` VARCHAR(255) NOT NULL");
+    $sql->execute('Столбец time не был создан.');
+
+    $sql = new sql();
     $sql->query("ALTER TABLE `number2meter` CHANGE COLUMN `service_id` `service` ENUM('hot_water', 'cold_water', 'electrical') NOT NULL");
     $sql->execute('Столбец rates не был создан.');
 
