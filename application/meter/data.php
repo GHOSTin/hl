668be04deae489828554b251d1ff769f36a0c306
nekrasov <nekrasov@mlsco.ru>
2013-06-08 13:29:54 +0600

    Урезал функциональность для счетчиков.

--- application/meter/data.php
+++ application/meter/data.php
@@ -9,15 +9,15 @@ final class data_meter extends data_object{
     public $company_id;
     public $id;
     public $name;
-    public $rates;
-    public $serial;
-    public $service;
-    public $period;
+    // public $serial;
     public $periods;
-    public $date_release;
-    public $date_install;
-    public $date_checking;
-    public $place;
+    public $rates;
+    public $service;
+    // public $period;
+    // public $date_release;
+    // public $date_install;
+    // public $date_checking;
+    // public $place;
 
     public function __construct(){
         if(empty($this->service))
