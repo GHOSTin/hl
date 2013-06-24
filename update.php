c165e0e43169cb7a465405c98e37f4d50b925684
nekrasov <nekrasov@mlsco.ru>
2013-06-24 17:28:03 +0600

    Сделал добавление столбца status  привязки счетчиков.

--- update.php
+++ update.php
@@ -1,2 +1,13 @@
 <?php
-die('Nothing update.'.PHP_EOL);
+# подключаем фреймворк
+define('ROOT' , __DIR__);
+require_once(ROOT."/framework/framework.php");
+
+try{
+    model_environment::create_batabase_connection();
+    $sql = new sql();
+    $sql->query("ALTER TABLE `number2meter` add COLUMN `status` ENUM('enabled', 'disabled') NOT NULL AFTER `meter_id`");
+    $sql->execute('Столбец status не был создан.');
+}catch(exception $e){
+    die('Обновление завершилось неудачно.');
+}
