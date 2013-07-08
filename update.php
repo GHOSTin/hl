f73931e5d7c813b9270b89c19a6e6fd80dbab7f7
nekrasov <nekrasov@mlsco.ru>
2013-07-08 14:30:07 +0600

    Сделал в обновлении добавление двух столбцов в показания счетчиков.

--- update.php
+++ update.php
@@ -1,2 +1,18 @@
 <?php
-die('Nothing update.'.PHP_EOL);
+// подключаем фреймворк
+define('ROOT' , __DIR__);
+require_once(ROOT."/framework/framework.php");
+
+try{
+    model_environment::create_batabase_connection();
+    $sql = new sql();
+    $sql->query("ALTER TABLE `meter2data` ADD COLUMN `way` ENUM('answerphone', 'fax', 'personally', 'telephone') NOT NULL AFTER `value`");
+    $sql->execute('Столбец way не был создан.');
+
+    $sql = new sql();
+    $sql->query("ALTER TABLE `meter2data` ADD COLUMN `timestamp` INT UNSIGNED NOT NULL AFTER `time`");
+    $sql->execute('Столбец timestamp не был создан.');
+
+}catch(exception $e){
+    die('Обновление завершилось неудачно.');
+}
