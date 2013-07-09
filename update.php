7170250e6c42518659bf03ce8e23b30204b8eb61
nekrasov <nekrasov@mlsco.ru>
2013-07-09 12:34:53 +0600

    Сделал в обновление добавления таблицы процессинговых центров.

--- update.php
+++ update.php
@@ -13,6 +13,15 @@ try{
     $sql->query("ALTER TABLE `meter2data` ADD COLUMN `timestamp` INT UNSIGNED NOT NULL AFTER `time`");
     $sql->execute('Столбец timestamp не был создан.');
 
+    $sql = new sql();
+    $sql->query("CREATE TABLE `processing_centers`(
+            `id` TINYINT UNSIGNED NOT NULL,
+            `name` VARCHAR(255) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+    $sql->execute('Таблица processing_centers не была создана.');
+
+
 }catch(exception $e){
+    die($e->getMessage());
     die('Обновление завершилось неудачно.');
 }
