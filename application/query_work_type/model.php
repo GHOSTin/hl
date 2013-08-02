82c2fca1aeac05ef6f135d24e79378c5b0986652
nekrasov <nekrasov@mlsco.ru>
2013-08-02 17:16:07 +0600

    Изменил верификацию для типа работ.

--- application/query_work_type/model.php
+++ application/query_work_type/model.php
@@ -17,34 +17,7 @@ class model_query_work_type{
 		}
 		return $sql->map(new data_query_work_type(), 'Проблема при выборке типов заявки.');
 	}
-	/**
-	* Верификация идентификатора компании.
-	*/
-	public static function verify_company_id(data_query_work_type $query_work_type){
-		if($query_work_type->company_id < 1)
-			throw new e_model('Идентификатор компании задан не верно.');
-	}
-	/**
-	* Верификация идентификатора типа работа заявки.
-	*/
-	public static function verify_id(data_query_work_type $query_work_type){
-		if($query_work_type->id < 1)
-			throw new e_model('Идентификатор типа заявки задан не верно.');
-	}
-	/**
-	* Верификация названия типа работ заявки.
-	*/
-	public static function verify_name(data_query_work_type $query_work_type){
-		if(empty($query_work_type->name))
-			throw new e_model('Название типа работ задано не верно.');
-	}
-	/**
-	* Верификация статуса типа работ заявки.
-	*/
-	public static function verify_status(data_query_work_type $query_work_type){
-		if(empty($query_work_type->status))
-			throw new e_model('Статус типа работ задано не верно.');
-	}
+
 	/**
 	* Проверка принадлежности объекта к классу data_query_work_type.
 	*/
