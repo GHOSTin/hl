d8b792ed77ce1c8566940bc3920c0394ebf14a90
nekrasov <nekrasov@mlsco.ru>
2013-08-02 17:34:17 +0600

    Подправил верификацию процессингового центра.

--- application/processing_center2number/verify.php
+++ application/processing_center2number/verify.php
@@ -5,8 +5,9 @@ class verify_processing_center2number{
     * Верификация идентификатора компании.
     */
     public static function company_id(data_processing_center2number $c2n){
-        if($c2n->company_id < 1)
-            throw new e_model('Идентификатор компании задан не верно.');
+        $company = new data_company();
+        $company->id = $c2n->company_id;
+        $company->verify('id');
     }
 
     /**
@@ -21,8 +22,9 @@ class verify_processing_center2number{
     * Верификация идентификатора лицевого счета.
     */
     public static function number_id(data_processing_center2number $c2n){
-        if($c2n->number_id < 1)
-            throw new e_model('Идентификатор лицевого счета задан не верно.');
+        $number = new data_number();
+        $number->id = $c2n->number_id;
+        $number->verify('id');
     }
 
     /**
