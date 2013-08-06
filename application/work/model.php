2fcbc31631ce583285efab16275d9663a0a8bba4
nekrasov <nekrasov@mlsco.ru>
2013-08-06 15:47:30 +0600

    Исправил ошибку в работах после верификации.

--- application/work/model.php
+++ application/work/model.php
@@ -11,7 +11,7 @@ class model_work{
 					WHERE `company_id` = :company_id");
 		$sql->bind(':company_id', $company->id, PDO::PARAM_INT);
 		if(!empty($work->id)){
-			model_work::verify_id($work);
+			$work->verify('id');
 			$sql->query(" AND `id` = :id");
 			$sql->bind(':id', $work->id, PDO::PARAM_INT);
 		}
