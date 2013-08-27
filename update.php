d195c8b2f209415ff4ef45c4ef9cb6cbf87c4fe8
nekrasov <nekrasov@mlsco.ru>
2013-08-27 15:41:12 +0600

    Сделал добавление таблицы соответствия дома и процессингового центра.

--- update.php
+++ update.php
@@ -1,2 +1,14 @@
 <?php
-die('Nothing update.'.PHP_EOL);
+define('ROOT' , __DIR__);
+require_once(ROOT."/framework/framework.php");
+model_environment::create_batabase_connection();
+
+$sql = new sql();
+$sql->query('CREATE TABLE `house2processing_center`(
+    `company_id` TINYINT UNSIGNED NOT NULL,
+    `house_id` SMALLINT UNSIGNED NOT NULL,
+    `center_id` TINYINT UNSIGNED NOT NULL,
+    `identifier` VARCHAR(16) NOT NULL,
+    KEY `id`(`company_id`, `house_id`)
+    )Engine=InnoDB DEFAULT CHARSET=utf8');
+$sql->execute('Проблемы при создании таблицы связей дома и просессингового центра.');
