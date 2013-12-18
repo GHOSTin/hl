fbd81117fc1fa4d03d17fd1128bb38d0378b9701
nekrasov <nekrasov@mlsco.ru>
2013-12-18 16:04:32 +0600

    Доудалял функциональность счетчика.

--- application/meter/mapper.php
+++ application/meter/mapper.php
@@ -7,17 +7,6 @@ class mapper_meter{
    `service`, `periods` FROM `meters`
     WHERE `company_id` = :company_id AND `id` = :id";
 
-  private static $one_by_name = "SELECT `id`, `company_id`, `name`, `capacity`,
-    `rates`, `service`, `periods` FROM `meters`
-    WHERE `company_id` = :company_id AND `name` = :name";
-
-  private static $insert = "INSERT INTO `meters` (`id`, `company_id`, `name`,
-    `rates`, `capacity`,`periods`) VALUES (:id, :company_id, :name, :rates,
-    :capacity, :periods)";
-
-  private static $insert_id = "SELECT MAX(`id`) as `max_id` FROM `meters`
-    WHERE `company_id` = :company_id";
-
   private static $all_by_service = "SELECT `id`, `company_id`, `name`,
     `capacity`, `rates`, `service`, `periods` FROM `meters`
     WHERE `company_id` = :company_id AND FIND_IN_SET(:service, `service`) > 0
@@ -27,29 +16,9 @@ class mapper_meter{
     `service`, `periods` FROM `meters`
     WHERE `company_id` = :company_id ORDER BY `name`";
 
-  private static $update = 'UPDATE `meters` SET `name` = :name,
-    `capacity` = :capacity, `rates` = :rates, `periods` = :periods,
-    `service` = :services WHERE `company_id` = :company_id AND `id` = :id';
-
   public function __construct(data_company $company){
     $this->company = $company;
     data_company::verify_id($this->company->get_id());
-  }
-
-  public function create(data_meter $meter){
-    $meter->set_id($this->get_insert_id());
-    $meter->set_company_id($this->company->get_id());
-    $meter->verify('id', 'company_id', 'name', 'rates', 'capacity');
-    $sql = new sql();
-    $sql->query(self::$insert);
-    $sql->bind(':id', (int) $meter->get_id(), PDO::PARAM_INT);
-    $sql->bind(':company_id', (int) $meter->get_company_id(), PDO::PARAM_INT);
-    $sql->bind(':name', (string) $meter->get_name(), PDO::PARAM_STR);
-    $sql->bind(':rates', (int) $meter->get_rates(), PDO::PARAM_INT);
-    $sql->bind(':capacity', (int) $meter->get_capacity(), PDO::PARAM_INT);
-    $sql->bind(':periods', (string) implode(';', $meter->get_periods()), PDO::PARAM_STR);
-    $sql->execute('Проблемы при создании счетчика.');
-    return $meter;
   }
 
   public function create_object(array $row){
@@ -89,42 +58,8 @@ class mapper_meter{
       throw new e_model('Неожиданное количество возвращаемых счетчиков.');
   }
 
-  public function find_by_name($name){
-    $sql = new sql();
-    $sql->query(self::$one_by_name);
-    $sql->bind(':company_id', (int) $this->company->get_id(), PDO::PARAM_INT);
-    $sql->bind(':name', (string) $name, PDO::PARAM_STR);
-    $sql->execute('Проблема при запросе счетчика.');
-    $stmt = $sql->get_stm();
-    $count = $stmt->rowCount();
-    if($count === 0)
-      return null;
-    elseif($count === 1)
-      return $this->create_object($stmt->fetch());
-    else
-      throw new e_model('Неожиданное количество возвращаемых счетчиков.');
-  }
-
   /**
-  * Возвращает следующий для вставки идентификатор счетчика.
-  * @return int
-  */
-  private function get_insert_id(){
-      $this->company->verify('id');
-      $sql = new sql();
-      $sql->query(self::$insert_id);
-      $sql->bind(':company_id', $this->company->get_id(), PDO::PARAM_INT);
-      $sql->execute('Проблема при опредении следующего meter_id.');
-      if($sql->count() !== 1)
-          throw new e_model('Проблема при опредении следующего meter_id.');
-      $id = (int) $sql->row()['max_id'] + 1;
-      $sql->close();
-      return $id;
-  }
-
-  /**
-  * Возвращает список счетчиков
-  * @return array data_meter
+  * Возвращает список счетчиков.
   */
   public function get_meters_by_service($service){
     $sql = new sql();
@@ -139,34 +74,9 @@ class mapper_meter{
     return $meters;
   }
 
-  public function update(data_meter $meter){
-    $this->verify($meter);
-    $sql = new sql();
-    $sql->query(self::$update);
-    $sql->bind(':company_id', (int) $this->company->get_id(), PDO::PARAM_INT);
-    $sql->bind(':id', (int) $meter->get_id(), PDO::PARAM_INT);
-    $sql->bind(':name', (int) $meter->get_name(), PDO::PARAM_STR);
-    $sql->bind(':capacity', (int) $meter->get_capacity(), PDO::PARAM_INT);
-    $sql->bind(':rates', (int) $meter->get_rates(), PDO::PARAM_INT);
-    $sql->bind(':periods', (string) implode(';', $meter->get_periods()), PDO::PARAM_STR);
-    $sql->bind(':services', (string) implode(',', $meter->get_services()), PDO::PARAM_STR);
-    $sql->execute('Проблема при обновлении счетчика.');
-    return $meter;
-  }
-
-  private function verify(data_meter $meter){
-    data_meter::verify_id($meter->get_id());
-    data_meter::verify_name($meter->get_name());
-    data_meter::verify_capacity($meter->get_capacity());
-    data_meter::verify_rates($meter->get_rates());
-    if(!empty($meter->get_periods()))
-      foreach($meter->get_periods() as $period)
-        data_meter::verify_period($period);
-    if(!empty($meter->get_services()))
-      foreach($meter->get_services() as $service)
-        data_meter::verify_service($service);
-  }
-
+  /**
+  * Возвращает список счетчиков.
+  */
   public function get_meters(){
     $sql = new sql();
     $sql->query(self::$all);
--- application/meter/model.php
+++ application/meter/model.php
@@ -7,28 +7,6 @@ class model_meter{
 		$this->company = $company;
 	}
 
-	/**
-	* Добавляет в счетчик новую период поверки.
-	* @return data_meter
-	*/
-	public function add_period($id, $period){
-		$meter = $this->get_meter($id);
-		$meter->add_period($period);
-		$mapper = new mapper_meter($this->company);
-		return $mapper->update($meter);
-	}
-
-	/**
-	* Добавляет в счетчик новую услугу.
-	* @return data_meter
-	*/
-	public function add_service($id, $service){
-	  $meter = $this->get_meter($id);
-		$meter->add_service($service);
-		$mapper = new mapper_meter($this->company);
-		return $mapper->update($meter);
-	}
-
 	/*
 	* Возвращает объект счетчика или падает с исключением, что счетчик не существует.
 	*/
@@ -53,82 +31,5 @@ class model_meter{
 	*/
 	public function get_meters_by_service($service){
 	    return (new mapper_meter($this->company))->get_meters_by_service($service);
-	}
-
-	/**
-	* Создает новый счетчик.
-	* @return data_meter
-	*/
-	public function create_meter($name, $capacity, $rates){
-		$mapper = new mapper_meter($this->company);
-		if(!is_null($mapper->find_by_name($name)))
-			throw new e_model('Счетчик с таким именем уже существует');
-		$meter = new data_meter();
-		$meter->set_name($name);
-		$meter->set_capacity($capacity);
-		$meter->set_rates($rates);
-		return $mapper->create($meter);
-	}
-
-	/**
-	* Исключает период
-	* @return data_meter
-	*/
-	public function remove_period($id, $period){
-	  $meter = $this->get_meter($id);
-		$meter->remove_period($period);
-		$mapper = new mapper_meter($this->company);
-		return $mapper->update($meter);
-	}
-
-	/**
-	* Удаляет привязку услуги к счетчику.
-	* @return data_meter
-	*/
-	public function remove_service($id, $service){
-	  $meter = $this->get_meter($id);
-		$meter->remove_service($service);
-		$mapper = new mapper_meter($this->company);
-		return $mapper->update($meter);
-	}
-
-	/**
-	* Переименовывает счетчик.
-	* @return data_meter
-	*/
-	public function rename_meter($id, $name){
-		$meter = $this->get_meter($id);
-		$mapper = new mapper_meter($this->company);
-		$old_meter = $mapper->find_by_name($name);
-		if(!is_null($old_meter))
-			if($meter->get_id() != $old_meter->get_id())
-				throw new e_model('Счетчик с таким именем уже существует.');
-		$meter->set_name($name);
-		$mapper->update($meter);
-		return $meter;
-	}
-
-	/**
-	* Изменяет разрядность счетчика.
-	* @return object data_meter
-	*/
-	public function update_capacity($id, $capacity){
-		$mapper = new mapper_meter($this->company);
-		$meter = $this->get_meter($id);
-		$meter->set_capacity($capacity);
-		$mapper->update($meter);
-		return $meter;
-	}
-
-	/**
-	* Изменяет тарифность счетчика.
-	* @return object data_meter
-	*/
-	public function update_rates($id, $rates){
-	  $mapper = new mapper_meter($this->company);
-		$meter = $this->get_meter($id);
-		$meter->set_rates($rates);
-		$mapper->update($meter);
-		return $meter;
 	}
 }
