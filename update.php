238bf75703c6ed98cd3ad35186ee815204c54dc1
nekrasov <nekrasov@mlsco.ru>
2014-02-11 11:23:15 +0600

    Ввел уникальный ключ для таблицы client_queries.

--- update.php
+++ update.php
@@ -8,5 +8,6 @@ $stmt = $pdo->exec('CREATE TABLE `client_queries` (
   `company_id` TINYINT(3) UNSIGNED NOT NULL,
   `number_id` MEDIUMINT(8) NOT NULL,
   `time` INT(10) UNSIGNED NOT NULL,
-  `text` VARCHAR(255)
-) Engine=InnoDB DEFAULT CHARSET=utf8');
+  `text` VARCHAR(255),
+  UNIQUE KEY `id` (`company_id`, `number_id`, `time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8');
