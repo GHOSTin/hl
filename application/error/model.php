dc03651e7cf92a4aa48079236517305e8ab112c1
nekrasov <nekrasov@mlsco.ru>
2014-03-07 15:17:32 +0600

    Сделал чтобы не возникало двойных сообщений об ошибке.

--- application/error/model.php
+++ application/error/model.php
@@ -11,8 +11,13 @@ class model_error{
   }
 
   public function send_error($text){
-    $e = ['text' => $text, 'user' => di::get('user'), 'time' => time()];
-    return di::get('mapper_error')->insert(di::get('factory_error')->build($e));
+    $time = time();
+    $user = di::get('user');
+    $mapper = di::get('mapper_error');
+    $e = ['text' => $text, 'user' => $user, 'time' => $time];
+    if(!is_null($mapper->find($time, $user->get_id())))
+      throw new RuntimeException();
+    return $mapper->insert(di::get('factory_error')->build($e));
   }
 
   public function get_errors(){
