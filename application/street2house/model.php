9ad7bd04d75d7defebb69031ded2a3bcf4dbab35
nekrasov <nekrasov@mlsco.ru>
2014-03-13 16:26:50 +0600

    Поправил ошибку в создании дома при импорте.

--- application/street2house/model.php
+++ application/street2house/model.php
@@ -17,7 +17,8 @@ class model_street2house{
     $mapper = new mapper_street2house($this->street);
     if(!is_null($mapper->find_by_number($house_number)))
       throw new e_model('Такой дом уже существует.');
-    $h_array = ['id' => $mapper->get_insert_id(), 'number' => $house_number];
+    $h_array = ['id' => $mapper->get_insert_id(), 'number' => $house_number,
+    'street' => $this->street];
     $house = di::get('factory_house')->build($h_array);
     return $mapper->insert($house);
   }
