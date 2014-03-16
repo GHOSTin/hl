83339f95d2a7849c705bdb359a066fbd865bd8e1
nekrasov <nekrasov@mlsco.ru>
2014-03-16 17:53:22 +0600

    Создал таблицу для логгирования входов.

--- update.php
+++ update.php
@@ -1,2 +1,13 @@
 <?php
-die('Nothing update.'.PHP_EOL);
+define('ROOT' , __DIR__);
+require_once(ROOT."/framework/framework.php");
+date_default_timezone_set(application_configuration::php_timezone);
+model_environment::before();
+$pdo = di::get('pdo');
+$pdo->exec("CREATE TABLE `sessions_logs`(
+  `user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `time` INT(10) UNSIGNED NOT NULL,
+  `ip` VARCHAR(15) NOT NULL,
+  KEY(`user_id`)
+  )Engine=InnoDB DEFAULT CHARSET=utf8");
+var_dump($pdo);
