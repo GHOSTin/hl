97fa95120df667e6b77855200a3209dd606b5ac0
nekrasov <nekrasov@mlsco.ru>
2014-03-17 15:45:07 +0600

    Затер обновления.

--- update.php
+++ update.php
@@ -1,13 +1,2 @@
 <?php
-define('ROOT' , __DIR__);
-require_once(ROOT."/framework/framework.php");
-date_default_timezone_set(application_configuration::php_timezone);
-model_environment::before();
-$pdo = di::get('pdo');
-$pdo->exec("CREATE TABLE `sessions_logs`(
-  `user_id` SMALLINT(5) UNSIGNED NOT NULL,
-  `time` INT(10) UNSIGNED NOT NULL,
-  `ip` VARCHAR(15) NOT NULL,
-  KEY(`user_id`)
-  )Engine=InnoDB DEFAULT CHARSET=utf8");
-var_dump($pdo);
+die('Nothing update.'.PHP_EOL);
