e8b1e8dd7d44feb765e0f99c6da36e386a949c0a
nekrasov <nekrasov@mlsco.ru>
2014-04-25 15:05:59 +0600

    Уьрал обновления. Удалил запускальщик тестов.

--- run_test.sh
+++
@@ -1,1 +0,0 @@
-phpunit --bootstrap tests/autoload.php tests/
--- update.php
+++ update.php
@@ -1,25 +1,2 @@
 <?php
-define('ROOT' , __DIR__);
-require_once(ROOT."/framework/framework.php");
-date_default_timezone_set(application_configuration::php_timezone);
-model_environment::before();
-$pdo = di::get('pdo');
-$pdo->exec('
-  CREATE TABLE IF NOT EXISTS accruals (
-    company_id TINYINT(3) UNSIGNED NOT NULL,
-    number_id MEDIUMINT(8) UNSIGNED NOT NULL,
-    time INT(10) UNSIGNED NOT NULL,
-    service VARCHAR(255) NOT NULL,
-    tarif decimal(9,3) NOT NULL,
-    ind decimal(9,3) NOT NULL,
-    odn decimal(9,3) NOT NULL,
-    sum_ind decimal(9,3) NOT NULL,
-    sum_odn decimal(9,3) NOT NULL,
-    recalculation decimal(9,3) NOT NULL,
-    facilities decimal(9,3) NOT NULL,
-    total decimal(9,3) NOT NULL,
-    KEY(company_id),
-    KEY(number_id),
-    KEY(time)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-');
+die('Nothing update.'.PHP_EOL);
