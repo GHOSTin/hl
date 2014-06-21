f7b4f98f70e1f67399196e45d6f965b29ccc11a7
GHOST home <lwhiteghostl@gmail.com>
2014-06-21 21:40:13 +0600

    добавил таблицу БД в update

--- update.php
+++ update.php
@@ -4,20 +4,9 @@ require_once(ROOT."/framework/framework.php");
 date_default_timezone_set(application_configuration::php_timezone);
 model_environment::before();
 $pdo = di::get('pdo');
-$pdo->exec("
-  CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` bigint(14) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `time_open` int(16) NOT NULL,
-  `time_close` int(16) DEFAULT NULL,
-  `time_target` int(16) NOT NULL,
-  `rating` int(5) DEFAULT NULL,
-  `reason` varchar(255) DEFAULT NULL,
-  `status` enum('open','close','reopen') DEFAULT 'open'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  CREATE TABLE IF NOT EXISTS `user2task` (
-    `task_id` bigint(14) NOT NULL,
-    `user_id` int(8) NOT NULL,
-    `user_type` enum('creator','performer') NOT NULL DEFAULT 'performer'
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-");
+$pdo->exec("CREATE TABLE IF NOT EXISTS `task2comment` (
+  `task_id` bigint(14) NOT NULL,
+  `user_id` smallint(5) NOT NULL,
+  `message` text NOT NULL,
+  `time` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
