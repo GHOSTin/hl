bd95554430662551c9b9d63c12b43e17523c712b
nekrasov <nekrasov@mlsco.ru>
2014-06-24 13:21:23 +0600

    Релиз.

--- update.php
+++ update.php
@@ -1,12 +1,2 @@
 <?php
-define('ROOT' , __DIR__);
-require_once(ROOT."/framework/framework.php");
-date_default_timezone_set(application_configuration::php_timezone);
-model_environment::before();
-$pdo = di::get('pdo');
-$pdo->exec("CREATE TABLE IF NOT EXISTS `task2comment` (
-  `task_id` bigint(14) NOT NULL,
-  `user_id` smallint(5) NOT NULL,
-  `message` text NOT NULL,
-  `time` int(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+die('Nothing update.'.PHP_EOL);
