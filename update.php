26191c7a9c68f72c4bdeb20a1e9bc2a9db73e7ef
GHOST home <lwhiteghostl@gmail.com>
2014-07-02 16:51:35 +0600

    Изменен update для добавления title в базу

--- update.php
+++ update.php
@@ -1,2 +1,7 @@
 <?php
-die('Nothing update.'.PHP_EOL);
+define('ROOT' , __DIR__);
+require_once(ROOT."/framework/framework.php");
+date_default_timezone_set(application_configuration::php_timezone);
+model_environment::before();
+$pdo = di::get('pdo');
+$pdo->exec("ALTER TABLE `tasks` ADD `title` TEXT NOT NULL AFTER `id`");
