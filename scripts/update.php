5805989a884e1e2a801d55f894e3bd6722dfe24e
nekrasov <nekrasov@mlsco.ru>
2014-07-18 15:48:38 +0600

    Рефакторинг.

--- scripts/update.php
+++ scripts/update.php
@@ -1,7 +1,2 @@
 <?php
-define('ROOT' , __DIR__);
-require_once(ROOT."/framework/framework.php");
-date_default_timezone_set(application_configuration::php_timezone);
-model_environment::before();
-$pdo = di::get('pdo');
-$pdo->exec("ALTER TABLE `tasks` ADD `title` TEXT NOT NULL AFTER `id`");
+die('Nothing update.'.PHP_EOL);
