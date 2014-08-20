3f9586ed50274d0a3d872a6c5fefc2f7de2502aa
GHOST home <lwhiteghostl@gmail.com>
2014-08-20 23:28:43 +0600

    Добавлен sql в update

--- scripts/update.php
+++ scripts/update.php
@@ -4,4 +4,8 @@ require_once(ROOT."/framework/framework.php");
 date_default_timezone_set(application_configuration::php_timezone);
 model_environment::before();
 $pdo = di::get('pdo');
-$pdo->exec("ALTER TABLE `tasks` ADD `title` TEXT NOT NULL AFTER `id`");
+$pdo->exec("CREATE TABLE IF NOT EXISTS `metrics` (
+  `id` varchar(128) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `metrics` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
