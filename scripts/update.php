208b44d691b2165bbfa227ab0b93969faad6be7e
nekrasov <nekrasov@mlsco.ru>
2014-08-23 10:13:57 +0600

    Рефакторинг.

--- scripts/update.php
+++ scripts/update.php
@@ -2,7 +2,6 @@
 define('ROOT' , substr(__DIR__, 0, (strlen(__DIR__) - strlen('/scripts'))));
 require_once(ROOT."/framework/framework.php");
 require_once(ROOT."/vendor/autoload.php");
-date_default_timezone_set(application_configuration::php_timezone);
 model_environment::before();
 $pdo = di::get('pdo');
 $pdo->beginTransaction();
