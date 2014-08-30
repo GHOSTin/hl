00f4e6be279d4c9843d2eaf081665caeabee02f2
nekrasov <nekrasov@mlsco.ru>
2014-08-31 01:13:48 +0600

    Сделал удаление идентификатора компании.

--- scripts/update.php
+++ scripts/update.php
@@ -6,12 +6,35 @@ model_environment::before();
 $pdo = di::get('pdo');
 $pdo->beginTransaction();
 drop_table($pdo);
+drop_company_id($pdo);
 update_users($pdo);
 $pdo->commit();
 
 function drop_table(PDO $pdo){
-  $pdo->exec("DROP table companies, sessions, phrases, sms, sms2number, sms2query, sms2user, smsgroup2number, smsgroups, processing_centers, processing_center2number, house2processing_center, materials, materialgroups, meters, meter2data, number2meter, query_close_reasons");
-  // $pdo->exec("UPDATE companies SET name = 'Наш город' WHERE id = 2");
+  $pdo->exec("DROP table companies, sessions, phrases, sms, sms2number, sms2query, sms2user, smsgroup2number, smsgroups, processing_centers, processing_center2number, house2processing_center, materials, materialgroups, meters, meter2data, number2meter, query_close_reasons, query2material");
+  $pdo->exec("UPDATE companies SET name = 'Наш город' WHERE id = 2");
+}
+
+function drop_company_id(PDO $pdo){
+  $pdo->exec("ALTER TABLE accruals DROP company_id");
+  $pdo->exec("ALTER TABLE cities DROP company_id");
+  $pdo->exec("ALTER TABLE client_queries DROP company_id");
+  $pdo->exec("ALTER TABLE departments DROP company_id");
+  $pdo->exec("ALTER TABLE flats DROP company_id");
+  $pdo->exec("ALTER TABLE groups DROP company_id");
+  $pdo->exec("ALTER TABLE houses DROP company_id");
+  $pdo->exec("ALTER TABLE numbers DROP company_id");
+  $pdo->exec("ALTER TABLE profiles DROP company_id");
+  $pdo->exec("ALTER TABLE queries DROP company_id");
+  $pdo->exec("ALTER TABLE query2comment DROP company_id");
+  $pdo->exec("ALTER TABLE query2number DROP company_id");
+  $pdo->exec("ALTER TABLE query2user DROP company_id");
+  $pdo->exec("ALTER TABLE query2work DROP company_id");
+  $pdo->exec("ALTER TABLE query_worktypes DROP company_id");
+  $pdo->exec("ALTER TABLE streets DROP company_id");
+  $pdo->exec("ALTER TABLE users DROP company_id");
+  $pdo->exec("ALTER TABLE workgroups DROP company_id");
+  $pdo->exec("ALTER TABLE works DROP company_id");
 }
 
 function update_users(PDO $pdo){
