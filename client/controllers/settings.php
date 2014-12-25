456ba0baded01b26a665011cfb64bcdf55828c6f
Nekrasov Eugene <nekrasov@mlsco.ru>
2014-12-26 00:36:03 +0500

    Оптимизация.

--- client/controllers/settings.php
+++ client/controllers/settings.php
@@ -9,25 +9,19 @@ class settings{
   public function change_password(Request $request, Application $app){
     $password = $request->get('new_password');
     $confirm = $request->get('confirm_password');
-    if($password !== $confirm)
-      return $app['twig']->render(
-        'settings/change_password.tpl',
-        ['number' => $app['number'], 'description' => 'Пароли не совпадают.', 'type' => 'error']
-      );
-    if($app['number']->get_hash() === number::generate_hash($request->get('old_password'), $app['salt'])){
-      $new_hash = number::generate_hash($password, $app['salt']);
-      $app['number']->set_hash($new_hash);
-      $app['em']->flush();
-      return $app['twig']->render(
-        'settings/change_password.tpl',
-        ['number' => $app['number'], 'description' => 'Пароль изменен.', 'type' => 'success']
-      );
+    if($password !== $confirm){
+      $response = ['number' => $app['number'], 'description' => 'Пароли не совпадают.', 'type' => 'error'];
     }else{
-      return $app['twig']->render(
-        'settings/change_password.tpl',
-        ['number' => $app['number'], 'description' => 'Старый пароль указан не верно.', 'type' => 'error']
-      );
+      if($app['number']->get_hash() === number::generate_hash($request->get('old_password'), $app['salt'])){
+        $new_hash = number::generate_hash($password, $app['salt']);
+        $app['number']->set_hash($new_hash);
+        $app['em']->flush();
+        $response = ['number' => $app['number'], 'description' => 'Пароль изменен.', 'type' => 'success'];
+      }else{
+        $response = ['number' => $app['number'], 'description' => 'Старый пароль указан не верно.', 'type' => 'error'];
+      }
     }
+    return $app['twig']->render('settings/change_password.tpl', $response);
   }
 
   public function default_page(Application $app){
