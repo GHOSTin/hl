15679fac37edacd3abc731bd949476d718761373
Nekrasov Eugene <nekrasov@mlsco.ru>
2015-08-17 14:16:07 +0500

    Обновление библиотек фронтенда. ТЕстыт на систему.

--- main/controllers/system.php
+++ main/controllers/system.php
@@ -4,11 +4,11 @@ use Silex\Application;
 
 class system{
 
-  public function default_page(Application $app){
-    return $app['main\models\system']->default_page();
-  }
-
   public function config(Application $app){
     return $app['main\models\system']->config();
+  }
+
+  public function default_page(Application $app){
+    return $app['main\models\system']->default_page();
   }
 }
