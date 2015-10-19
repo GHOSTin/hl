0a0f1a6e7bcc67c975350cdfef6127ff59cc8d9f
Nekrasov Eugene <nekrasov@mlsco.ru>
2015-10-19 12:44:49 +0500

    Создание отключений.

--- main/templates/outages/default_page.tpl
+++ main/templates/outages/default_page.tpl
@@ -1,1 +1,29 @@
-<h2>Отключения</h2>
+<p>
+  <a class="get_dialog_create_outage">Создать</a>
+</p>
+<ul>
+  {% for outage in outages %}
+    <li>
+        <div>c {{ outage.get_begin()|date("d.m.Y") }} по {{ outage.get_target()|date("d.m.Y") }} {{ outage.get_category().get_name() }}: {{ outage.get_description() }}</div>
+        <div>Диспетчер: {{ outage.get_user().get_fio() }}</div>
+        <div class="row">
+          <div class="col-md-6">
+            <h5>Дома</h5>
+            <ul>
+              {% for house in outage.get_houses() %}
+              <li>{{ house.get_address() }} </li>
+              {% endfor %}
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <h5>Исполнители</h5>
+            <ul>
+              {% for user in outage.get_performers() %}
+              <li>{{ user.get_fio() }} </li>
+              {% endfor %}
+            </ul>
+          </div>
+        </div>
+    </li>
+  {% endfor %}
+</ul>
--- tests/main_models_outages_Test.php
+++ tests/main_models_outages_Test.php
@@ -30,11 +30,96 @@ class main_model_outages_Test extends PHPUnit_Framework_TestCase{
                ->method('check_access')
                ->with('numbers/general_access')
                ->willReturn(true);
+    $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+                       ->disableOriginalConstructor()
+                       ->setMethods(['findAll'])
+                       ->getMock();
+    $repository->expects($this->once())
+               ->method('findAll')
+               ->willReturn('outages_array');
+    $this->em->expects($this->once())
+               ->method('getRepository')
+               ->with('domain\outage')
+               ->willReturn($repository);
     $this->twig->expects($this->once())
                ->method('render')
-               ->with('outages\default_page.tpl')
+               ->with('outages/default_page.tpl', ['outages' => 'outages_array'])
                ->willReturn('render_template');
     $model = new model($this->twig, $this->em, $this->user, 125);
     $this->assertEquals(['workspace' => 'render_template'], $model->default_page());
+  }
+
+  public function test_dialog_create(){
+    $this->user->expects($this->once())
+               ->method('check_access')
+               ->with('numbers/general_access')
+               ->willReturn(true);
+    $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+                       ->disableOriginalConstructor()
+                       ->getMock();
+    $this->em->expects($this->exactly(3))
+               ->method('getRepository')
+               ->withConsecutive(
+                                  ['domain\workgroup'],
+                                  ['domain\street'],
+                                  ['domain\group']
+                                )
+               ->willReturn($repository);
+    $this->twig->expects($this->once())
+               ->method('render')
+               ->with('outages/dialog_create.tpl')
+               ->willReturn('render_template');
+    $model = new model($this->twig, $this->em, $this->user, 125);
+    $this->assertEquals('render_template', $model->dialog_create());
+  }
+
+  public function test_houses(){
+    $this->user->expects($this->once())
+               ->method('check_access')
+               ->with('numbers/general_access')
+               ->willReturn(true);
+    $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+                       ->disableOriginalConstructor()
+                       ->setMethods(['findOneById'])
+                       ->getMock();
+    $repository->expects($this->once())
+               ->method('findOneById')
+               ->with(250)
+               ->willReturn('street_object');
+    $this->em->expects($this->once())
+               ->method('getRepository')
+               ->with('domain\street')
+               ->willReturn($repository);
+    $this->twig->expects($this->once())
+               ->method('render')
+               ->with('outages/houses.tpl', ['street' => 'street_object'])
+               ->willReturn('render_template');
+    $model = new model($this->twig, $this->em, $this->user, 125);
+    $this->assertEquals('render_template', $model->houses(250));
+  }
+
+  public function test_users(){
+    $this->user->expects($this->once())
+               ->method('check_access')
+               ->with('numbers/general_access')
+               ->willReturn(true);
+    $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+                       ->disableOriginalConstructor()
+                       ->setMethods(['findOneById'])
+                       ->getMock();
+    $repository->expects($this->once())
+               ->method('findOneById')
+               ->with(250)
+               ->willReturn('group_object');
+    $this->em->expects($this->once())
+               ->method('getRepository')
+               ->with('domain\group')
+               ->willReturn($repository);
+    $this->twig->expects($this->once())
+               ->method('render')
+               ->with('outages/users.tpl', ['group' => 'group_object'])
+               ->willReturn('render_template');
+    $model = new model($this->twig, $this->em, $this->user, 125);
+    $this->assertEquals('render_template', $model->users(250));
   }
 }
