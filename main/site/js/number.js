9633d501542f79d750a869f1c0c0e4657001f26d
nemytov <lwhiteghostl@gmail.com>
2015-12-25 15:41:15 +0500

    Клиентская часть добавления файла в созданном событии.

--- main/site/js/number.js
+++ main/site/js/number.js
@@ -1,3 +1,55 @@
+var AddedFilesView = Backbone.Marionette.ItemView.extend({
+    events: {
+        'click .close_dialog': 'close_dialog',
+        'click .added_files': 'add_files'
+    },
+    template: Twig.twig({
+        href: '/templates/events/build_added_files.tpl',
+        async: false
+    }),
+    className: 'modal-dialog add_files',
+    render: function() {
+        $(this.el).html(this.template.render());
+    },
+    onShow: function(){
+        var model = this.model;
+        this.dropZone = $("#events-files").dropzone({
+            url: '/files/',
+            autoProcessQueue: false,
+            uploadMultiple: true,
+            parallelUploads: 100,
+            maxFiles: 100,
+            addRemoveLinks: true,
+            dictDefaultMessage: '<div class="btn btn-primary">Прикрепить файлы</div>',
+            previewTemplate: $('#preview-template').html(),
+            dictRemoveFile: 'Открепить'
+        });
+        this.dropZone[0].dropzone.on('successmultiple', function(files, res) {
+            model.files = _.union(model.files, res);
+            $.ajax({
+                url: '/numbers/events/' + model.id + '/',
+                method: 'PUT',
+                data: model
+            }).done(function(res){
+                var template = Twig.twig({
+                    href: '/templates/numbers/event.tpl',
+                    async: false
+                });
+                $('.event[event_id = ' + model.id + ']').replaceWith(template.render({"n2e": model}));
+                MyApp.modal.hideModal();
+            });
+        });
+    },
+    close_dialog: function(){
+        MyApp.modal.hideModal();
+    },
+    add_files: function(){
+        if(this.dropZone[0].dropzone.getQueuedFiles().length > 0) {
+            this.dropZone[0].dropzone.processQueue();
+        }
+    }
+});
+
 $(document).ready(function(){
 
     $('body').on('click', '.get_street_content', function(){
@@ -163,6 +215,14 @@ $(document).ready(function(){
           });
     }).on('click', '.remove_element',  function(){
       $(this).parent().remove();
+    })
+    .on('click', '.get_dialog_added_files', function(){
+        var event_id = $(this).closest('.event').attr('event_id');
+        $.get('/numbers/events/' + event_id + '/')
+            .done(function(res){
+                var AddedFiles = new AddedFilesView({"model": res.event});
+                MyApp.modal.show(AddedFiles);
+            });
     });
 
 });
--- main/site/templates/events/build_added_files.tpl
+++ main/site/templates/events/build_added_files.tpl
@@ -1,10 +1,10 @@
-{% extends "dialog.tpl" %}
+{% extends "../dialog.tpl" %}
 {% block title %}Добавление файлов{% endblock title %}
 {% block dialog %}
-  <form id="my-awesome-dropzone" class="dropzone" action="#">
+  <form id="events-files" class="dropzone" action="#">
     <div class="dropzone-previews"></div>
   </form>
 {% endblock dialog %}
 {% block buttons %}
-  <div class="btn btn-default update_cellphone">Сохранить</div>
+  <div class="btn btn-primary added_files">Загрузить</div>
 {% endblock buttons %}
